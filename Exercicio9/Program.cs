ed2dad84518d8bcaccb0cc14cc27607839f9e8e0
Éric Salvi <ericsalvi67@hotmail.com>
2025-01-08 13:13:53 -0300

    Adiciona novo projeto e manipulação de dicionário
    Foi adicionado um novo arquivo de projeto `Exercicio9.csproj` com a configuração para usar o SDK do .NET, definir o tipo de saída como executável, o framework de destino como .NET 8.0, habilitar o uso implícito de namespaces e habilitar a verificação de nulidade.
    
    No arquivo `Program.cs`, foi adicionado um dicionário `Dictionary<int, string>` chamado `estados` com alguns estados brasileiros. O código verifica se a chave `4` não está presente no dicionário e, se não estiver, adiciona "Santa Catarina". Em seguida, o código itera sobre os valores do dicionário e imprime cada estado. Por fim, tenta obter o valor associado à chave `3` e imprime esse valor se a chave existir.

---
+++ Exercicio9/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Dictionary<int,string> estados = new Dictionary<int, string>();
+estados.Add(1, "São Paulo");
+estados.Add(2, "Rio de Janeiro");
+estados.Add(3, "Minas Gerais");
+
+if(!estados.ContainsKey(4))
+{
+    estados.Add(4, "Santa Catarina");
+}
+
+foreach (string estado in estados.Values)
+{
+    Console.WriteLine(estado);
+}
+
+string est = null;
+if(estados.TryGetValue(3, out est))
+{
+    Console.WriteLine(est);
+}
