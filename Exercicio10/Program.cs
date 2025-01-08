3a6260d9cb8de1825b5d406b5c2dfd910aa303f0
Éric Salvi <ericsalvi67@hotmail.com>
2025-01-08 13:13:35 -0300

    Adiciona configuração do projeto e modifica lógica em Program.cs
    Adiciona o arquivo Exercicio10.csproj com configuração do projeto,
    incluindo SDK do .NET, tipo de saída como executável, framework
    de destino .NET 8.0, e habilitação das diretivas ImplicitUsings
    e Nullable.
    
    Modifica o arquivo Program.cs para comentar o código de ordenação
    e exibição de uma lista de inteiros, e adiciona novo código que
    trabalha com uma fila (Queue) de strings. O novo código adiciona
    três strings à fila, exibe os elementos, remove o primeiro
    elemento, exibe os elementos restantes, e exibe o primeiro
    elemento da fila sem removê-lo.

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+/*List<int> numeros = new List<int>() {1,0,9,6,3,5,2,8,7,4 };
+numeros.Sort();
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}*/
+
+Queue<string> numeros = new Queue<string>();
+numeros.Enqueue("ola");
+numeros.Enqueue("mundo");
+numeros.Enqueue("!");
+
+
+foreach (string palavra in numeros)
+{
+    Console.WriteLine(palavra);
+}
+
+Console.WriteLine("-------------------------------------");
+
+numeros.Dequeue(); //remove o primeiro elemento da fila
+
+foreach (string palavra in numeros)
+{
+    Console.WriteLine(palavra);
+}
+
+int total = numeros.Count;
+
+Console.WriteLine("-------------------------------------");
+Console.WriteLine(numeros.Peek()); //mostra o primeiro elemento da fila
