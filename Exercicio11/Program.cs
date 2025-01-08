f15eaa7df6616e51ddfadf047c01420d9d47c03c
Éric Salvi <ericsalvi67@hotmail.com>
2025-01-08 13:23:41 -0300

    Adiciona projeto .NET 8.0 e manipulação de pilha em C#
    Foi adicionado um novo arquivo de projeto `Exercicio11.csproj` com a configuração para um projeto .NET 8.0, incluindo a habilitação de usings implícitos e a verificação de nulidade.
    
    Foi adicionado um novo código no arquivo `Program.cs` que cria e manipula uma pilha (`Stack<string>`) de strings. O código adiciona três strings à pilha, imprime todos os itens, remove e imprime o último item adicionado, exibe o último item sem removê-lo e, finalmente, imprime a quantidade total de itens na pilha.

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Stack<string> lista = new Stack<string>(); // Cria uma pilha de strings, conceito de LIFO (Last In First Out)
+lista.Push("Ola");
+lista.Push("Mundo");
+lista.Push("!");
+
+foreach (var item in lista)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("-----------------------");
+Console.WriteLine(lista.Pop()); // Remove o ultimo item adicionado
+Console.WriteLine("-----------------------");
+Console.WriteLine(lista.Peek()); // Retorna o ultimo item adicionado sem remove-lo
+Console.WriteLine("-----------------------");
+int total = lista.Count; // Retorna a quantidade de itens na pilha
