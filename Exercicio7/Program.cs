149f940ba9f70feace3c33add45a29898f3a1f33
Éric Salvi <ericsalvi67@hotmail.com>
2025-01-08 12:32:33 -0300

    Adiciona novo projeto e inicializa array em Program.cs
    No arquivo `Exercicio7.csproj`, foi adicionado um novo projeto com o SDK do .NET, configurado para gerar um executável (`Exe`) e direcionado para o framework `net8.0`. As opções `ImplicitUsings` e `Nullable` foram habilitadas.
    
    No arquivo `Program.cs`, foi adicionado um código que inicializa um array de inteiros `v` com valores de 0 a 5. Um loop `foreach` foi adicionado para iterar sobre os elementos do array e imprimir cada número no console. Após o loop `foreach`, uma mensagem "Fim do loop foreach\n" é exibida. Finalmente, um loop `for` foi adicionado para iterar sobre os elementos do array e imprimir cada número no console.

---
+++ Exercicio7/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int[] v = { 0, 1, 2, 3, 4, 5};
+
+foreach (int numero in v) //similar ao for ou while, mas mais simples
+{
+    Console.WriteLine(numero);
+}
+Console.WriteLine("Fim do loop foreach\n");
+
+for (int i = 0; i < v.Length; i++)
+{
+    Console.WriteLine(v[i]);
+}
