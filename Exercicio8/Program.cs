e0d8be7a6c473b603f0e9ec0a1e835c500d706eb
Éric Salvi <ericsalvi67@hotmail.com>
2025-01-08 13:13:02 -0300

    Adiciona configuração do projeto e exemplo de arrays/listas
    Adicionado `Exercicio8.csproj` com configuração do projeto:
    - SDK do .NET
    - Tipo de saída como executável
    - Framework de destino .NET 8.0
    - Usings implícitas habilitadas
    - Verificação de nulidade habilitada
    
    Adicionado `Program.cs` com exemplo de manipulação de arrays e listas:
    - Inicialização de array de inteiros
    - Impressão do primeiro elemento do array
    - Criação, adição e remoção de elementos em listas
    - Contagem e obtenção de elementos específicos
    - Iteração e limpeza de listas

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+int[] ints = new int[] {1,2,3,4,5};
+int num = ints[0];
+
+Console.WriteLine($"{num}\n"); // 1
+
+int total = ints.Length;
+
+List <int> list = new List<int>();
+List<int> list2 = new List<int>() {1,2,3,4,5};
+
+int count = list2.Count;
+list2.Add(6);
+list2.Remove(3);
+
+
+int num2 = list2.ElementAt(2);
+Console.WriteLine($"{num2}\n");
+
+foreach (int i in list2)
+{
+    Console.WriteLine(i);
+}
+
+list2.Clear();
