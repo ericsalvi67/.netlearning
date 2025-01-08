360760a27022fe2563db8a939e613e8467932109
Salvi <Salvi@SALVI-DOTSKOP>
2025-01-08 10:47:47 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,26 @@
+﻿//numericos
+int i = 10;
+float f = 5.76f; //4 bytes
+double d = 5.76d; //8 bytes
+decimal dc = 7.32m; //16 bytes
+
+//booleanos
+bool b = true; //true or false
+
+//caracteres
+char c = 'a';
+string s = "Hello World";
+
+//variaveis implicitas
+var v1 = 10; //int
+var v2 = 5.76f; //float
+var v3 = 5.76d; //double
+var v4 = 7.32m; //decimal
+var v5 = "cidade"; //string
+
+//constantes
+const int icons = 10;
+const float fcons = 5.76f;
+const double dcons = 5.76d;
+const decimal dccons = 7.32m;
+//valores fixos para constantes e eles nao serao alterados
---
+++ Exercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿//operadores atiméticos
+int add = 10 + 5;
+int sub = 10 - 5;
+int mult = 10 * 5;
+int div = 10 / 5;
+int resto = 10 % 5;
+
+//operadores relacionais (comparadores de variaveis numericas,strings,etc)
+bool maior = 10 > 5;
+bool menorIgual = 10 <= 5;
+bool menor = 10 < 5;
+bool maiorIgual = 10 >= 5;
+bool igual = 10 == 5;
+bool diferente = 10 != 5;
+
+//operadores lógicos
+bool and = true && false;
+bool or = true || false;
+bool not = !true;
+
+
+
+
